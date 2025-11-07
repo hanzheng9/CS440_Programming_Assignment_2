99c855359ba40a281aca85e2c0b118f736fe7579
Han Zheng <hanzheng@Hans-MacBook-Air-2.local>
2025-11-07 16:56:40 -0500

    finished move ordering

--- pa2/src/pas/othello/ordering/MoveOrderer.java
+++ pa2/src/pas/othello/ordering/MoveOrderer.java
@@ -3,7 +3,9 @@ package src.pas.othello.ordering;
 
 // SYSTEM IMPORTS
 import edu.bu.pas.othello.traversal.Node;
+import src.pas.othello.heuristics.Heuristics;
 
+import java.util.Comparator;
 import java.util.List;
 
 
@@ -18,6 +20,11 @@ public class MoveOrderer
     public static List<Node> orderChildren(List<Node> children)
     {
         // TODO: complete me!
+        if(children==null || children.isEmpty()) 
+            return children;
+
+        children.sort(Comparator.comparingDouble((Node n) -> Heuristics.calculateHeuristicValue(n)).reversed());
+
         return children;
     }
 
